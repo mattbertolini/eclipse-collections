b5889033890f7029cb481437e1596cf24cb42fe9
Matt Bertolini <viper2843@gmail.com>
2024-12-03 21:04:27 -0500

    Start adding Javadoc

--- eclipse-collections-assertj/src/main/java/org/eclipse/collections/assertj/api/multimap/AbstractMultimapAssert.java
+++ eclipse-collections-assertj/src/main/java/org/eclipse/collections/assertj/api/multimap/AbstractMultimapAssert.java
@@ -39,6 +39,19 @@ public abstract class AbstractMultimapAssert<SELF extends AbstractMultimapAssert
         throw this.assertionError(shouldHaveSize(this.actual, actualSize, expected));
     }
 
+    /**
+     * Verifies that the {@link Multimap} is empty.
+     * <p>
+     * Example:
+     * <pre>{@code // assertion will pass
+     * assertThat(Multimaps.mutable.list.empty()).isEmpty();
+     *
+     * // assertion will fail
+     * Multimap<String, String> multimap = Multimaps.mutable.list.with("Key", "Value");
+     * assertThat(multimap).isEmpty();}</pre>
+     *
+     * @throws AssertionError if the {@link Multimap} of values is not empty.
+     */
     public void isEmpty()
     {
         this.isNotNull();
